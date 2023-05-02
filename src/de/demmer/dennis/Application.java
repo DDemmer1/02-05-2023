596c4809829147d2f47b73ec0ee539316b6e86fa
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-05-02 09:14:57 +0200

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,8 +1,17 @@
 package de.demmer.dennis;
 
+
+//denkt an den import
+import java.util.Random;
+
 public class Application {
 	
 	public static void main(String[] args) {
+		
+		//zufällige Zahl von 1-100
+		int zufallsZahl = new Random().nextInt(101);
+
+		
 		
 	}
 
